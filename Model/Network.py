321b3453abca8561ee5ef701c7c518848fe9f962
Laura Quiroga <l.quiroga@uniandes.edu.co>
2023-04-03 19:14:09 -0500

    Implement Euler's method to solve model V1

--- Model/Network.py
+++ Model/Network.py
@@ -1,6 +1,6 @@
 import numpy as np
 import networkx as nx
-from .States import States
+from .states import States
 
 class Network:
     def __init__(self, adjMatrix: np.array, init_states: list) -> None:
@@ -10,32 +10,13 @@ class Network:
             # adjMatrix[i][j] = 1 , i and j are neighbours
             # adjMatrix[i][j] = 0 , i and j are not neighbours
         self.adjMatrix = adjMatrix
-        # graph:= graph that represents the network
-        self.graph = nx.Graph()
 
-        # add the nodes and edges to the nx graph
-        for i in range(self.n):
-            for j in range(i+1, self.n): #since network is undirected graph
-                if self.adjMatrix[i, j] == 1:
-                    self.graph.add_edge(i,j)
+        # graph:= graph that represents the network (created from adjMatrix)
+        self.graph = nx.Graph(adjMatrix)
         
+
+        """
         # initialize states
         for node in range(self.n):
             nx.set_node_attributes(self.graph, init_states[node], 'state')
-
-
-    # Funtion that finds list of infected neighbours for the given node(source)
-    def get_infected_neigbours(self, source:int) -> list:
-        neighbours = []
-        #find neighbours
-        for j in range(self.n):
-            if self.adjMatrix[source][j]:
-                neighbours.append(j)
-
-        resp = []
-        #find if infected
-        for i in neighbours:
-            if self.graph.nodes[i]['state'] == States.I.value:
-                resp.append(i)
-
-        return resp
+        """
--- Model/SEIRS_Model.py
+++ Model/SEIRS_Model.py
@@ -1,37 +1,75 @@
 import numpy as np
 import random
-from .Network import Network
+from .network import Network
 
 class SEIRS_Model:
-    # Model definition and initialization
-    def __init__(self, network: Network, rates) -> None:
+    # ------------------ Model definition and initialization -------------------
+    def __init__(self, network: Network, iterations: int, **rates) -> None:
         # network:= 
         self.network = network
-        # iterations:=
-        self.iterations = 0
-        # n:= number of devices (nodes) in the network
-        self.n = network.n
+
         # rates := probabilities to change of state
         self.parameters = rates
 
-    # Function that executes the model simulation
+        # n_times:= number of time-steps considered
+        self.n_times = iterations
+
+        # x := probability of being in the S state for every node
+        # w := probability of being in the E state for every node
+        # y := probability of being in the I state for every node
+        # z := probability of being in the R state for every node
+        self.x = np.ones((self.n, iterations)) # all nodes start in susceptible state
+        self.w = np.zeros((self.n, iterations))
+        self.y = np.zeros((self.n, iterations))
+        self.z = np.zeros((self.n, iterations))
+        # !!!!!!!!!!!!!por ahora lo voy a inicializar en un nodo cualquiera
+        self.x[3,0] = 0
+        self.y[3, 0] = 1  # 4th node has the virus initially
+
+    # ------------ Definition of the system of differential equations ------------
+
+    # b(t) = beta* summation over j of: [(a_ij)*y_i(t)]       //using the first order approximation
+
+    def w_prime(self, w, y, z, i):
+        """
+        The rate of change of the probability of the node (i) being in the Exposed state:
+        w'(t) = (1-w(t)-y(t)-z(t))*b(t) - alpha*w(t)
+        """
+        return (1-w[i]-y[i]-z[i])*np.dot(self.network.adjMatrix[i,:], y) - self.rates["alpha"]*w[i]
+
+    def y_prime(self, w, y, i):
+        """
+        The rate of change of the probability of the node (i) being in the Infected state:
+        y'(t) =  alpha*w(t) - delta*y(t)
+        """
+        return self.rates["alpha"] * w[i] - self.rates["delta"] * y[i]
+
+    def z_prime(self, y, z, i):
+        """
+        The rate of change of the probability of the node (i) being in the Recovered state:
+        z'(t) = delta*y(t) - gamma*z(t)
+        """
+        return self.rates["delta"] * y[i] - self.rates["gamma"] * z[i]
+
     def run_model(self) -> None:
-        t = 0
-        while t< self.iterations:
+        """
+        Executes the model simulation
+        """
+        min_t = 0
+        max_t = self.n_times/100
+        t = np.linspace(min_t, max_t, self.n_times)
+        dt = t[1] - t[0]
+        
+        # The arrays are filled in the for loop following the formula
+        # Numeric solution to the ODE using Euler's method
+        for k in range(1, self.n_times):
+            t[k] = t[k - 1] + dt
+            for i in range(self.network.n):
+                self.w[i, k] = self.w[i, k - 1] + dt * self.w_prime( self.w[:, k - 1], self.y[:, k - 1],  self.z[:, k - 1], i)
+                self.y[i, k] = self.y[i, k - 1] + dt * self.y_prime( self.w[:, k - 1], self.y[:, k - 1],  i)
+                self.z[i, k] = self.z[i, k - 1] + dt * self.z_prime( self.y[:, k - 1], self.z[:, k - 1], i)
+                self.x[i, k] = 1 - self.y[i, k] - self.z[i, k] - self.w[i, k]
+                if (self.x[i, k]<0):
+                    self.x[i, k]=0
 
-            # for each node in the network
-            for node in range(self.n):
-                # S nodes (Susceptible)
-
-                # E nodes (Exposed)
-
-                # I nodes (Infected)
-
-                # R nodes (Recovered)
-
-                pass
-
-            t+=1
-
-    def calculate_beta(self):
-        pass
+        
